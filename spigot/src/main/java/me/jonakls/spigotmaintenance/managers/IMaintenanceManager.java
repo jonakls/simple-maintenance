9267745bc2846fe00fc0f1bdd5f76774ea94acc0
jonakls <asmot54@hotmail.com>
2022-06-26 22:49:55 -0500

    feat: created parent class of MaintenanceManager for generate interaction with platform configuration

---
+++ spigot/src/main/java/me/jonakls/spigotmaintenance/managers/IMaintenanceManager.java
@@ -0,0 +1,16 @@
+package me.jonakls.spigotmaintenance.managers;
+
+import me.jonakls.core.manager.MaintenanceManager;
+
+public class IMaintenanceManager extends MaintenanceManager {
+
+    @Override
+    public void saveWhitelist() {
+        super.saveWhitelist();
+    }
+
+    @Override
+    public void loadWhitelist() {
+        super.loadWhitelist();
+    }
+}
