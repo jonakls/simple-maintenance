697157d03ad340a73f16cd16504aaa4e139e5c5d
jonakls <asmot54@hotmail.com>
2022-06-26 22:48:26 -0500

    feat: created normal file of configuration for spigot platform, added instances of managers, listeners and commands

--- spigot/src/main/java/me/jonakls/spigotmaintenance/SimpleMaintenanceSpigot.java
+++ spigot/src/main/java/me/jonakls/spigotmaintenance/SimpleMaintenanceSpigot.java
@@ -1,16 +1,36 @@
 package me.jonakls.spigotmaintenance;
 
+import me.jonakls.spigotmaintenance.listener.PlayerPreCheckListener;
+import me.jonakls.spigotmaintenance.managers.IMaintenanceManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public class SimpleMaintenanceSpigot extends JavaPlugin {
 
+    private IMaintenanceManager maintenanceManager;
+
     @Override
     public void onEnable() {
-        super.onEnable();
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+
+        // Init maintenance manager system
+        maintenanceManager = new IMaintenanceManager();
+
+        // Register listeners
+        getServer().getPluginManager().registerEvents(new PlayerPreCheckListener(this), this);
+
+        // Register commands
+        Objects.requireNonNull(getCommand("maintenance")).setExecutor(new MaintenanceCommand(this));
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    public IMaintenanceManager getMaintenanceManager() {
+        return maintenanceManager;
     }
 }
