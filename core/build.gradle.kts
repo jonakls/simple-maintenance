4397132d045bddbabe7370d0a7d8fbaa5e55fec0
jonakls <asmot54@hotmail.com>
2022-06-26 22:46:29 -0500

    build: added compatibility on java 8 and added dependency of jetbrains annotations and files matching

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,8 +1,21 @@
 plugins {
-    id("java")
+    java
     id("com.github.johnrengelman.shadow") version("7.1.2")
 }
 
 repositories {
     mavenCentral()
+    maven("https://oss.sonatype.org/content/groups/public/")
+
+}
+
+dependencies {
+    implementation("org.jetbrains:annotations:23.0.0")
+}
+
+tasks {
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -10,5 +10,24 @@ repositories {
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.18-R0.1-SNAPSHOT")
+    implementation("org.jetbrains:annotations:23.0.0")
+
     compileOnly(project(":core"))
+}
+
+tasks {
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    processResources {
+        filesMatching("**/*.yml") {
+            filter<org.apache.tools.ant.filters.ReplaceTokens>(
+                "tokens" to mapOf(
+                    "version" to project.version
+                )
+            )
+        }
+    }
 }
